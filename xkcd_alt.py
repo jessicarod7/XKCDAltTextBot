7ed12bc2d282b450d04798596f086c5dfdb0aae2
Cameron Rodriguez <rod.cam2014+dev@gmail.com>
2018-11-18 20:43:22 -0500

    created main program and .gitignore files

---
+++ xkcd_alt.py
@@ -0,0 +1,4 @@
+"""This is the XKCD Alt Text Bot.
+
+This bot checks once a minute for new Tweets from @xkcdComic. If one is found, it accesses the
+linked comic, extracts the image alt text, and Tweets it as a reply."""
